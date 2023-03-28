bc2e5cf0e0f76430ae62cf878a64948a32411ab1
Parsa Mohammadian <parsa2820@gmail.com>
2023-03-28 13:19:06 +0330

    Implement useradd command

---
+++ src/useradd_command.sh
@@ -0,0 +1,22 @@
+username=${args[--username]}
+if [[ -z $username ]]; then
+    username=$(openssl rand -hex 16)
+fi
+password=${args[--password]}
+if [[ -z $password ]]; then
+    password=$(openssl rand -hex 16)
+fi
+expire=${args[--expire]}
+traffic=${args[--traffic]}
+
+encrypted=$(openssl passwd -1 $password)
+
+result=$(useradd -e $expire -M -s /bin/sh -p $encrypted $username)
+if [[ $result == "useradd: user '$username' already exists" ]]; then
+    echo "user already exists"
+    exit 1
+fi
+
+echo "User created successfully"
+echo "Username: $username"
+echo "Password: $password"
