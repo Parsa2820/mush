610c1ef91f3ea613756196cdc1ad8be81f69c994
Parsa Mohammadian <parsa2820@gmail.com>
2023-04-03 23:24:41 +0330

    Implement userdel

--- src/userdel_command.sh
+++ src/userdel_command.sh
@@ -1,4 +1,14 @@
-echo "# this file is located in 'src/userdel_command.sh'"
-echo "# code for 'mush userdel' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+username=${args[username]}
+
+if [[ -z $username ]]; then
+    echo "username is required"
+    exit 1
+fi
+
+result=$(userdel -r $username)
+if [[ $result == "userdel: user '$username' does not exist" ]]; then
+    echo "user does not exist"
+    exit 1
+fi
+
+echo "User deleted successfully"
